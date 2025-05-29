9aa7af879b8232691a853938ca00c3bc9c4d6884
Steve Mares <stevee_mares@hotmail.com>
2025-05-28 21:41:52 -0500

    added buttons with useState hooks

--- card-component/src/App.jsx
+++ card-component/src/App.jsx
@@ -1,4 +1,7 @@
-import Card from "./Card.jsx"
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+/* import Card from "./Card.jsx"
 
 function App() {
  
@@ -14,3 +17,49 @@ function App() {
 }
 
 export default App
+
+*/
+
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < sculptureList.length - 1;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+    console.log(showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
